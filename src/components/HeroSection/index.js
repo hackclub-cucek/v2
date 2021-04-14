2e19059ff54b65fac52a6ca102774343e683fb08
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-04-15 00:43:16 +0530

    Font Added

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import Video from '../../videos/video.mp4';
+import { Button } from '../ButtonElements';
+import { 
+    HeroContainer, 
+    HeroBg, 
+    VideoBg, 
+    HeroContent, 
+    HeroH1,
+    HeroP,
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight
+} from './HeroElements';
+
+const HeroSection = () => {
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Hack Club CUCEK</HeroH1>
+                <HeroP>
+                    Looking for a tech group accepted worldwide from our college?
+                    You just got into the right place.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button 
+                    to='join' 
+                    onMouseEnter={onHover}
+                    onMouseLeave={onHover}
+                    primary='true'
+                    dark='true'
+                    >
+                        Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
