7484a2ec5155f6e4405027683fc60b9834bbb00a
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-04-14 15:36:27 +0530

    Nav

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa' 
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElement';
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/' >j4ck</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='discover'>Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='contact'>Contact</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='join'>Join Now</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to='/join'>Join Now</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
